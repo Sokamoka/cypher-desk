b1e87dcbf483fdf6f782641b6a0664b70d4ad9d5
sokamoka <akos.stegner@gmail.com>
2026-08-09 15:39:46 +0200

    Add drizzle studio

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,7 +1,10 @@
-import { defineConfig } from 'drizzle-kit';
+import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
-  schema: './server/database/schema.ts',
-  out: './server/database/migrations',
-  dialect: 'sqlite',
+  schema: "./server/database/schema.ts",
+  out: "./server/database/migrations",
+  dialect: "sqlite",
+  dbCredentials: {
+    url: ".wrangler/state/v3/d1/miniflare-D1DatabaseObject/bc26c27a00cf6f0e45b7ca09b0beff73a7e386128e7488194d07b3cfd2ab58e2.sqlite",
+  },
 });
